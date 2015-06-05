09281dabe8c56de768043a1b82ef771c8ad699e2
Jonathan Simcoe <jonathan@simko.io>
2015-06-05 16:23:29 -0700

    Initial commit

---
+++ src/pages/repos.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ampersandMixin from 'ampersand-react-mixin'
+
+export default React.createClass({
+  mixins: [ampersandMixin],
+
+  displayName: 'ReposPage',
+
+  render () {
+    const me = app.me
+    const {repos} = this.props
+
+    return (
+      <div>
+        <h2>{me.name}&#8217;s repos</h2>
+        <h4 className="account-details">
+          <a href="{me.blog}">{me.blog}</a>
+          <br/>
+          {me.location}
+        </h4>
+        <div className="repos">
+          {repos.map((repo) => {
+            return (
+              <div>
+                <a href="">{repo.full_name}</a>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+
+  }
+})
